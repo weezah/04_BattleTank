a0816640627d4bd81a31522ae22e4aa163cf651f
Mattia Ryffel <mattia.ryffel@disneyresearch.com>
2018-04-05 17:03:49 +0200

    BT58 A Better Component Architecture

---
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankMovementComponent.h"
+#include "TankTrack.h"
+
+
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+    if (!LeftTrackToSet || !RightTrackToSet)
+    {return;}
+    
+    LeftTrack = LeftTrackToSet;
+    RightTrack = RightTrackToSet;
+}
+
+void UTankMovementComponent::IntendMoveForward(float Throw)
+{
+    RightTrack->SetThrottle(Throw);
+    LeftTrack->SetThrottle(Throw);
+}
---
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/NavMovementComponent.h"
+#include "TankMovementComponent.generated.h"
+
+
+class UTankTrack;
+/**
+ * 
+ */
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+public:
+    UFUNCTION(BlueprintCallable)
+    void IntendMoveForward(float Throw);
+    UFUNCTION(BlueprintCallable)
+    void Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
+    
+private:
+    UTankTrack* LeftTrack = nullptr;
+    UTankTrack* RightTrack = nullptr;
+	
+};
